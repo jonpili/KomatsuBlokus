e5473703e4c0b8ec05b1cdf0540a5034dbcda4f4
JoeTakasuna <jonpili99@gmail.com>
2019-05-28 00:27:42 +0900

    bブロックの変数変更によるコード簡易化

--- pieces/b_block.py
+++ pieces/b_block.py
@@ -5,103 +5,103 @@ ABLESET = 2
 GREEN  = 1
 YELLOW = 2
 
-def changeTileStatus1(colorImage, colorRect, x, y, board1, board2, surface, tileLength):
+def changeTileStatus1(colorImage, colorRect, x, y, boardMine, boardOpponent, surface, tileLength):
     surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
     surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * (y+1)))
 
     # ブロック自体を左上から時計回りに
-    board1[y][x] = CANTSET
-    board1[y+1][x] = CANTSET
+    boardMine[y][x] = CANTSET
+    boardMine[y+1][x] = CANTSET
 
     # ブロックと辺で接する地点を左上から時計回りに
-    board1[y-1][x] = CANTSET
-    board1[y][x+1] = CANTSET
-    board1[y+1][x+1] = CANTSET
-    board1[y+2][x] = CANTSET
-    board1[y+1][x-1] = CANTSET
-    board1[y][x-1] = CANTSET
+    boardMine[y-1][x] = CANTSET
+    boardMine[y][x+1] = CANTSET
+    boardMine[y+1][x+1] = CANTSET
+    boardMine[y+2][x] = CANTSET
+    boardMine[y+1][x-1] = CANTSET
+    boardMine[y][x-1] = CANTSET
 
     # ブロックと角で接する地点を左上から時計回りに
-    if board1[y-1][x-1] != CANTSET:
-        board1[y-1][x-1] = ABLESET
+    if boardMine[y-1][x-1] != CANTSET:
+        boardMine[y-1][x-1] = ABLESET
 
-    if board1[y-1][x+1] != CANTSET:
-        board1[y-1][x+1] = ABLESET
+    if boardMine[y-1][x+1] != CANTSET:
+        boardMine[y-1][x+1] = ABLESET
 
-    if board1[y+2][x+1] != CANTSET:
-        board1[y+2][x+1] = ABLESET
+    if boardMine[y+2][x+1] != CANTSET:
+        boardMine[y+2][x+1] = ABLESET
 
-    if board1[y+2][x-1] != CANTSET:
-        board1[y+2][x-1] = ABLESET
+    if boardMine[y+2][x-1] != CANTSET:
+        boardMine[y+2][x-1] = ABLESET
 
     # ブロック自体を左上から時計回りに
-    board2[y][x] = CANTSET
-    board2[y+1][x] = CANTSET
+    boardOpponent[y][x] = CANTSET
+    boardOpponent[y+1][x] = CANTSET
 
-def changeTileStatus2(colorImage, colorRect, x, y, board1, board2, surface, tileLength):
+def changeTileStatus2(colorImage, colorRect, x, y, boardMine, boardOpponent, surface, tileLength):
     surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
     surface.blit(colorImage, colorRect.move(tileLength * (x-1), tileLength * y))
 
     # ブロック自体を左上から時計回りに
-    board1[y][x] = CANTSET
-    board1[y][x-1] = CANTSET
+    boardMine[y][x] = CANTSET
+    boardMine[y][x-1] = CANTSET
 
     # ブロックと辺で接する地点を左上から時計回りに
-    board1[y-1][x-1] = CANTSET
-    board1[y-1][x] = CANTSET
-    board1[y][x+1] = CANTSET
-    board1[y+1][x] = CANTSET
-    board1[y+1][x-1] = CANTSET
-    board1[y][x-2] = CANTSET
+    boardMine[y-1][x-1] = CANTSET
+    boardMine[y-1][x] = CANTSET
+    boardMine[y][x+1] = CANTSET
+    boardMine[y+1][x] = CANTSET
+    boardMine[y+1][x-1] = CANTSET
+    boardMine[y][x-2] = CANTSET
 
     # ブロックと角で接する地点を左上から時計回りに
-    if board1[y-1][x-2] != CANTSET:
-        board1[y-1][x-2] = ABLESET
+    if boardMine[y-1][x-2] != CANTSET:
+        boardMine[y-1][x-2] = ABLESET
 
-    if board1[y-1][x+1] != CANTSET:
-        board1[y-1][x+1] = ABLESET
+    if boardMine[y-1][x+1] != CANTSET:
+        boardMine[y-1][x+1] = ABLESET
 
-    if board1[y+1][x+1] != CANTSET:
-        board1[y+1][x+1] = ABLESET
+    if boardMine[y+1][x+1] != CANTSET:
+        boardMine[y+1][x+1] = ABLESET
 
-    if board1[y+1][x-2] != CANTSET:
-        board1[y+1][x-2] = ABLESET
+    if boardMine[y+1][x-2] != CANTSET:
+        boardMine[y+1][x-2] = ABLESET
 
     # ブロック自体を左上から時計回りに
-    board2[y][x] = CANTSET
-    board2[y][x-1] = CANTSET
+    boardOpponent[y][x] = CANTSET
+    boardOpponent[y][x-1] = CANTSET
 
-def settableCheck1(board):
-    if boardGreen[y][x] != CANTSET and boardGreen[y+1][x] != CANTSET:
-        if boardGreen[y][x] == ABLESET or boardGreen[y+1][x] == ABLESET:
+def settableCheck1(boardMine):
+    if boardMine[y][x] != CANTSET and boardMine[y+1][x] != CANTSET:
+        if boardMine[y][x] == ABLESET or boardMine[y+1][x] == ABLESET:
             return True
 
-def settableCheck2(board):
-    if boardGreen[y][x] != CANTSET and boardGreen[y][x-1] != CANTSET:
-        if boardGreen[y][x] == ABLESET or boardGreen[y][x-1] == ABLESET:
+def settableCheck2(boardMine):
+    if boardMine[y][x] != CANTSET and boardMine[y][x-1] != CANTSET:
+        if boardMine[y][x] == ABLESET or boardMine[y][x-1] == ABLESET:
             return True
 
-def main(color, colorImage, colorRect, selectedDirection, x, y, boardGreen, boardYellow, surface, tileLength):
+def main(color, colorImage, colorRect, boardMine, boardOpponent, selectedDirection, x, y, surface, tileLength):
     if color == GREEN:
         if selectedDirection == 1: # 初期向き（下）
-            if settableCheck1(boardGreen):
-                changeTileStatus1(colorImage, colorRect, x, y, boardGreen, boardYellow, surface, tileLength)
+            if settableCheck1(boardMine):
+                changeTileStatus1(colorImage, colorRect, x, y, boardMine, boardOpponent, surface, tileLength)
                 return True
 
         elif selectedDirection == 2: # 初期向きから90°時計回りに（左）
-            if settableCheck2(boardGreen):
-                changeTileStatus2(colorImage, colorRect, x, y, boardGreen, boardYellow, surface, tileLength)
+            if settableCheck2(boardMine):
+                changeTileStatus2(colorImage, colorRect, x, y, boardMine, boardOpponent, surface, tileLength)
                 return True
 
     elif color == YELLOW:
         if selectedDirection == 1: # 初期向き（下）
-            if settableCheck1(boardYellow):
-                changeTileStatus1(colorImage, colorRect, x, y, boardYellow, boardGreen, surface, tileLength)
+            if settableCheck1(boardMine):
+                changeTileStatus1(colorImage, colorRect, x, y, boardMine, boardOpponent, surface, tileLength)
                 return True
 
         elif selectedDirection == 2: # 初期向きから90°時計回りに（左）
-            if settableCheck2(boardYellow):
-                changeTileStatus2(colorImage, colorRect, x, y, boardYellow, boardGreen, surface, tileLength)
+            if settableCheck2(boardMine):
+                changeTileStatus2(colorImage, colorRect, x, y, boardMine, boardOpponent, surface, tileLength)
                 return True
 
 if __name__ == '__main__':
