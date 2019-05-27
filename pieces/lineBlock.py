8c0a2e7c06d8f9863c05e93e27d30e07c4b65d55
JoeTakasuna <jonpili99@gmail.com>
2019-05-27 23:50:03 +0900

    bブロックの画面表示コードの繰り返し削減

--- pieces/lineBlock.py
+++ pieces/lineBlock.py
@@ -5,7 +5,10 @@ ABLESET = 2
 GREEN  = 1
 YELLOW = 2
 
-def changeTileStatus1(x, y, board1, board2):
+def changeTileStatus1(colorImage, colorRect, x, y, board1, board2, surface, tileLength):
+    surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
+    surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * (y+1)))
+
     # ブロック自体を左上から時計回りに
     board1[y][x] = CANTSET
     board1[y+1][x] = CANTSET
@@ -35,7 +38,10 @@ def changeTileStatus1(x, y, board1, board2):
     board2[y][x] = CANTSET
     board2[y+1][x] = CANTSET
 
-def changeTileStatus2(x, y, board1, board2):
+def changeTileStatus2(colorImage, colorRect, x, y, board1, board2, surface, tileLength):
+    surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
+    surface.blit(colorImage, colorRect.move(tileLength * (x-1), tileLength * y))
+
     # ブロック自体を左上から時計回りに
     board1[y][x] = CANTSET
     board1[y][x-1] = CANTSET
@@ -67,37 +73,30 @@ def changeTileStatus2(x, y, board1, board2):
 
 def main(color, colorImage, colorRect, selectedDirection, x, y, boardGreen, boardYellow, surface, tileLength):
     if color == GREEN:
-        if selectedDirection == 1: # 下向き（図における初期向き）
+        if selectedDirection == 1: # 初期向き（下）
             if ((boardGreen[y][x] != CANTSET and boardGreen[y+1][x] != CANTSET)
             and (boardGreen[y][x] == ABLESET or boardGreen[y+1][x] == ABLESET)):
-                surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
-                surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * (y+1)))
-                changeTileStatus1(x, y, boardGreen, boardYellow)
+                changeTileStatus1(colorImage, colorRect, x, y, boardGreen, boardYellow, surface, tileLength)
                 return True
 
-        elif selectedDirection == 2: # 左向き（図における初期向きから時計回りに）
+        elif selectedDirection == 2: # 初期向きから90°時計回りに（左）
             if ((boardGreen[y][x] != CANTSET and boardGreen[y][x-1] != CANTSET)
             and (boardGreen[y][x] == ABLESET or boardGreen[y][x-1] == ABLESET)):
-                surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
-                surface.blit(colorImage, colorRect.move(tileLength * (x-1), tileLength * y))
-                changeTileStatus2(x, y, boardGreen, boardYellow)
+
+                changeTileStatus2(colorImage, colorRect, x, y, boardGreen, boardYellow, surface, tileLength)
                 return True
 
     elif color == YELLOW:
-        if selectedDirection == 1: # 下向き（図における初期向き）
+        if selectedDirection == 1: # 初期向き（下）
             if ((boardYellow[y][x] != CANTSET and boardYellow[y+1][x] != CANTSET)
             and (boardYellow[y][x] == ABLESET or boardYellow[y+1][x] == ABLESET)):
-                surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
-                surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * (y+1)))
-                changeTileStatus1(x, y, boardYellow, boardGreen)
+                changeTileStatus1(colorImage, colorRect, x, y, boardYellow, boardGreen, surface, tileLength)
                 return True
 
-        elif selectedDirection == 2: # 左向き（図における初期向きから時計回りに）
+        elif selectedDirection == 2: # 初期向きから90°時計回りに（左）
             if ((boardYellow[y][x] != CANTSET and boardYellow[y][x-1] != CANTSET)
             and (boardYellow[y][x] == ABLESET or boardYellow[y][x-1] == ABLESET)):
-                surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
-                surface.blit(colorImage, colorRect.move(tileLength * (x-1), tileLength * y))
-                changeTileStatus2(x, y, boardGreen, boardYellow)
+                changeTileStatus2(colorImage, colorRect, x, y, boardYellow, boardGreen, surface, tileLength)
                 return True
 
 if __name__ == '__main__':
