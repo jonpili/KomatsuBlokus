d948f2e08116a2ae1a0cda7bc39e81008bc8d9e7
JoeTakasuna <jonpili99@gmail.com>
2019-05-30 09:03:43 +0900

    転置や回転ができるようにブロックによる影響を行列で表現

--- pieces/b_block.py
+++ pieces/b_block.py
@@ -14,35 +14,23 @@ def changeTileImage1(colorImage, colorRect, x, y, surface, tileLength):
     surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
     surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * (y+1)))
 
-def changeTileStatus1(boardMine, boardOpponent, x, y):
-    # ブロック自体を左上から時計回りに
-    boardMine[y][x] = CANTSET
-    boardMine[y+1][x] = CANTSET
+def changeBoardStatus(blockShape, blockInfluences, boardMine, boardOpponent, x, y):
+    # ブロックの影響を自分のボードに適用
+    for i in range(len(blockInfluences)):
+        for j in range(len(blockInfluences)):
+            tileStatus = blockInfluences[i][j]
+            if tileStatus == CANTSET:
+                boardMine[y+i-2][x+j-2] = CANTSET
+            elif tileStatus == ABLESET:
+                if boardMine[y+i-2][x+j-2] != CANTSET:
+                    boardMine[y+i-2][x+j-2] = ABLESET
 
-    # ブロックと辺で接する地点を左上から時計回りに
-    boardMine[y-1][x] = CANTSET
-    boardMine[y][x+1] = CANTSET
-    boardMine[y+1][x+1] = CANTSET
-    boardMine[y+2][x] = CANTSET
-    boardMine[y+1][x-1] = CANTSET
-    boardMine[y][x-1] = CANTSET
-
-    # ブロックと角で接する地点を左上から時計回りに
-    if boardMine[y-1][x-1] != CANTSET:
-        boardMine[y-1][x-1] = ABLESET
-
-    if boardMine[y-1][x+1] != CANTSET:
-        boardMine[y-1][x+1] = ABLESET
-
-    if boardMine[y+2][x+1] != CANTSET:
-        boardMine[y+2][x+1] = ABLESET
-
-    if boardMine[y+2][x-1] != CANTSET:
-        boardMine[y+2][x-1] = ABLESET
-
-    # ブロック自体を左上から時計回りに
-    boardOpponent[y][x] = CANTSET
-    boardOpponent[y+1][x] = CANTSET
+    # ブロックの形を自分以外のボードに適用
+    for i in range(len(blockShape)):
+        for j in range(len(blockShape)):
+            tileStatus = blockShape[i][j]
+            if tileStatus == CANTSET:
+                boardOpponent[y+i-1][x+j-1] = CANTSET
 
 def settableCheck2(boardMine, x, y):
     if boardMine[y][x] != CANTSET and boardMine[y][x-1] != CANTSET:
@@ -84,10 +72,24 @@ def changeTileStatus2(boardMine, boardOpponent, x, y):
     boardOpponent[y][x-1] = CANTSET
 
 def main(colorImage, colorRect, boardMine, boardOpponent, selectedDirection, x, y, surface, tileLength):
+    blockShape = [
+    [0,0,0],
+    [0,1,0],
+    [0,1,0]
+    ]
+
+    blockInfluences = [
+    [0,0,0,0,0],
+    [0,2,1,2,0],
+    [0,1,1,1,0],
+    [0,1,1,1,0],
+    [0,2,1,2,0]
+    ]
+
     if selectedDirection == 1: # 初期向き
         if settableCheck1(boardMine, x, y):
             changeTileImage1(colorImage, colorRect, x, y, surface, tileLength)
-            changeTileStatus1(boardMine, boardOpponent, x, y)
+            changeBoardStatus(blockShape, blockInfluences, boardMine, boardOpponent, x, y)
             return True
 
     elif selectedDirection == 2: # 初期向きから90°時計回りに
