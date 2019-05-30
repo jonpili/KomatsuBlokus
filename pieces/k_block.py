2e9d556655de3f00e27d520cf55c4f36548f1651
JoeTakasuna <jonpili99@gmail.com>
2019-05-30 23:46:29 +0900

    numpy arrayの導入でコードを1/8に圧縮できるように

--- pieces/k_block.py
+++ pieces/k_block.py
@@ -7,172 +7,79 @@ ABLESET = 2
 GREEN  = 1
 YELLOW = 2
 
-def settableCheck1(boardMine, x, y):
-    if (boardMine[y][x] != CANTSET
-    and boardMine[y-1][x] != CANTSET
-    and boardMine[y-2][x] != CANTSET
-    and boardMine[y+1][x] != CANTSET
-    and boardMine[y+1][x-1] != CANTSET):
-        if boardMine[y][x] != ABLESET\
-        or boardMine[y-1][x] != ABLESET\
-        or boardMine[y-2][x] != ABLESET\
-        or boardMine[y+1][x] != ABLESET\
-        or boardMine[y+1][x-1] != ABLESET:
+def settableCheck(blockShape, boardMine, x, y):
+    # 1つでもCANTSETがあれば置けない
+    for coord in np.argwhere(blockShape == CANTSET):
+        if boardMine[y + coord[0] - 2][x + coord[1] - 2] == CANTSET:
+            return False
+    # 1つでもABLESETがあれば置ける
+    for coord in np.argwhere(blockShape == CANTSET):
+        if boardMine[y + coord[0] - 2][x + coord[1] - 2] == ABLESET:
             return True
 
-def changeTileImage1(colorImage, colorRect, x, y, surface, tileLength):
-    surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
-    surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * (y-1)))
-    surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * (y-2)))
-    surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * (y+1)))
-    surface.blit(colorImage, colorRect.move(tileLength * (x-1), tileLength * (y+1)))
+def changeBoardImage(blockShape, colorImage, colorRect, x, y, surface, tileLength):
+    for coord in np.argwhere(blockShape == CANTSET):
+        surface.blit(colorImage, colorRect.move(tileLength * (x + coord[1] - 2), tileLength * (y + coord[0] - 2)))
 
-def changeTileStatus1(boardMine, boardOpponent, x, y):
-    # ブロック自体を左上から時計回りに
-    boardMine[y][x] = CANTSET
-    boardMine[y-1][x] = CANTSET
-    boardMine[y-2][x] = CANTSET
-    boardMine[y+1][x] = CANTSET
-    boardMine[y+1][x-1] = CANTSET
+def changeBoardStatus(blockShape, blockInfluences, boardMine, boardOpponent, x, y):
+    # ブロックの影響を自分のボードに適用
+    for coord in np.argwhere(blockInfluences == CANTSET):
+        boardMine[y + coord[0] - 3][x + coord[1] - 3] = CANTSET
+    for coord in np.argwhere(blockInfluences == ABLESET):
+        if boardMine[y + coord[0] - 3][x + coord[1] - 3] == BLANK:
+            boardMine[y + coord[0] - 3][x + coord[1] - 3] = ABLESET
 
-    # ブロックと辺で接する地点を左上から時計回りに
-    boardMine[y-3][x] = CANTSET
-    boardMine[y-2][x+1] = CANTSET
-    boardMine[y-1][x+1] = CANTSET
-    boardMine[y][x+1] = CANTSET
-    boardMine[y+1][x+1] = CANTSET
-    boardMine[y+2][x] = CANTSET
-    boardMine[y+2][x-1] = CANTSET
-    boardMine[y+1][x-2] = CANTSET
-    boardMine[y][x-1] = CANTSET
-    boardMine[y-1][x-1] = CANTSET
-    boardMine[y-2][x-1] = CANTSET
-
-    # ブロックと角で接する地点を左上から時計回りに
-    if boardMine[y-3][x-1] != CANTSET:
-        boardMine[y-3][x-1] = ABLESET
-
-    if boardMine[y-3][x+1] != CANTSET:
-        boardMine[y-3][x+1] = ABLESET
-
-    if boardMine[y+2][x+1] != CANTSET:
-        boardMine[y+2][x+1] = ABLESET
-
-    if boardMine[y+2][x-2] != CANTSET:
-        boardMine[y+2][x-2] = ABLESET
-
-    if boardMine[y][x-2] != CANTSET:
-        boardMine[y][x-2] = ABLESET
-
-    # ブロック自体を左上から時計回りに
-    boardOpponent[y][x] = CANTSET
-    boardOpponent[y-1][x] = CANTSET
-    boardOpponent[y-2][x] = CANTSET
-    boardOpponent[y+1][x] = CANTSET
-    boardOpponent[y+1][x-1] = CANTSET
-
-def settableCheck2(boardMine, x, y):
-    if (boardMine[y][x] != CANTSET
-    and boardMine[y][x+1] != CANTSET
-    and boardMine[y][x+2] != CANTSET
-    and boardMine[y][x-1] != CANTSET
-    and boardMine[y-1][x-1] != CANTSET):
-        if boardMine[y][x] != ABLESET\
-        or boardMine[y][x+1] != ABLESET\
-        or boardMine[y][x+2] != ABLESET\
-        or boardMine[y][x-1] != ABLESET\
-        or boardMine[y-1][x-1] != ABLESET:
-            return True
-
-def changeTileImage2(colorImage, colorRect, x, y, surface, tileLength):
-    surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
-    surface.blit(colorImage, colorRect.move(tileLength * (x+1), tileLength * y))
-    surface.blit(colorImage, colorRect.move(tileLength * (x+2), tileLength * y))
-    surface.blit(colorImage, colorRect.move(tileLength * (x-1), tileLength * y))
-    surface.blit(colorImage, colorRect.move(tileLength * (x-1), tileLength * (y-1)))
-
-def changeTileStatus2(boardMine, boardOpponent, x, y):
-    # ブロック自体を左上から時計回りに
-    boardMine[y][x] = CANTSET
-    boardMine[y][x+1] = CANTSET
-    boardMine[y][x+2] = CANTSET
-    boardMine[y][x-1] = CANTSET
-    boardMine[y-1][x-1] = CANTSET
-
-    # ブロックと辺で接する地点を左上から時計回りに
-    boardMine[y-2][x-1] = CANTSET
-    boardMine[y-1][x] = CANTSET
-    boardMine[y-1][x+1] = CANTSET
-    boardMine[y-1][x+2] = CANTSET
-    boardMine[y][x+3] = CANTSET
-    boardMine[y+1][x+2] = CANTSET
-    boardMine[y+1][x+1] = CANTSET
-    boardMine[y+1][x] = CANTSET
-    boardMine[y+1][x-1] = CANTSET
-    boardMine[y][x-1] = CANTSET
-    boardMine[y-1][x-2] = CANTSET
-
-    # ブロックと角で接する地点を左上から時計回りに
-    if boardMine[y-2][x-2] != CANTSET:
-        boardMine[y-2][x-2] = ABLESET
-
-    if boardMine[y-2][x] != CANTSET:
-        boardMine[y-2][x] = ABLESET
-
-    if boardMine[y-1][x+3] != CANTSET:
-        boardMine[y-1][x+3] = ABLESET
-
-    if boardMine[y+1][x+3] != CANTSET:
-        boardMine[y+1][x+3] = ABLESET
-
-    if boardMine[y+1][x-2] != CANTSET:
-        boardMine[y+1][x-2] = ABLESET
-
-    # ブロック自体を左上から時計回りに
-    boardOpponent[y][x] = CANTSET
-    boardOpponent[y][x+1] = CANTSET
-    boardOpponent[y][x+2] = CANTSET
-    boardOpponent[y][x-1] = CANTSET
-    boardOpponent[y-1][x-1] = CANTSET
-
-def changeTileImage3(colorImage, colorRect, x, y, surface, tileLength):
-    surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
-    surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * (y-1)))
-    surface.blit(colorImage, colorRect.move(tileLength * (x+1), tileLength * (y-1)))
-    surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * (y+2)))
-    surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * (y+1)))
-
-def changeTileImage5(colorImage, colorRect, x, y, surface, tileLength):
-    surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
-    surface.blit(colorImage, colorRect.move(tileLength * (x-1), tileLength * y))
-    surface.blit(colorImage, colorRect.move(tileLength * (x-2), tileLength * y))
-    surface.blit(colorImage, colorRect.move(tileLength * (x+1), tileLength * y))
-    surface.blit(colorImage, colorRect.move(tileLength * (x+1), tileLength * (y-1)))
+    # ブロックの影響を自分以外のボードに適用
+    for coord in np.argwhere(blockShape == CANTSET):
+        boardOpponent[y + coord[0] - 2][x + coord[1] - 2] = CANTSET
 
 def main(colorImage, colorRect, boardMine, boardOpponent, selectedDirection, x, y, surface, tileLength):
+    blockShape = np.asarray([
+    [0,0,1,0,0],
+    [0,0,1,0,0],
+    [0,0,1,0,0],
+    [0,1,1,0,0],
+    [0,0,0,0,0]
+    ])
+
+    blockInfluences = np.asarray([
+    [0,0,2,1,2,0,0],
+    [0,0,1,1,1,0,0],
+    [0,0,1,1,1,0,0],
+    [0,2,1,1,1,0,0],
+    [0,1,1,1,1,0,0],
+    [0,2,1,1,2,0,0],
+    [0,0,0,0,0,0,0]
+    ])
+
     if selectedDirection == 1: # 初期向き
-        if settableCheck1(boardMine, x, y):
-            changeTileImage1(colorImage, colorRect, x, y, surface, tileLength)
-            changeTileStatus1(boardMine, boardOpponent, x, y)
+        if settableCheck(blockShape, boardMine, x, y):
+            changeBoardImage(blockShape, colorImage, colorRect, x, y, surface, tileLength)
+            changeBoardStatus(blockShape, blockInfluences, boardMine, boardOpponent, x, y)
             return True
 
     elif selectedDirection == 2: # 初期向きから90°時計回りに
-        if settableCheck2(boardMine, x, y):
-            changeTileImage2(colorImage, colorRect, x, y, surface, tileLength)
-            changeTileStatus2(boardMine, boardOpponent, x, y)
+        blockShape = np.rot90(blockShape, -1)
+        blockInfluences = np.rot90(blockInfluences, -1)
+        if settableCheck(blockShape, boardMine, x, y):
+            changeBoardImage(blockShape, colorImage, colorRect, x, y, surface, tileLength)
+            changeBoardStatus(blockShape, blockInfluences, boardMine, boardOpponent, x, y)
             return True
 
     elif selectedDirection == 3: # 初期向きから180°時計回りに
-        pass
+        blockShape = np.rot90(blockShape, -2)
+        blockInfluences = np.rot90(blockInfluences, -2)
+        if settableCheck(blockShape, boardMine, x, y):
+            changeBoardImage(blockShape, colorImage, colorRect, x, y, surface, tileLength)
+            changeBoardStatus(blockShape, blockInfluences, boardMine, boardOpponent, x, y)
+            return True
 
-    elif selectedDirection == 5: # 初期向きから裏向きに
-        boardMine = boardMine.T
-        boardOpponent = boardOpponent.T
-        if settableCheck1(boardMine, y, x):
-            changeTileImage5(colorImage, colorRect, y, x, surface, tileLength)
-            changeTileStatus1(boardMine, boardOpponent, y, x)
-            boardMine = boardMine.T
-            boardOpponent = boardOpponent.T
+    elif selectedDirection == 4: # 初期向きから270°時計回りに
+        blockShape = np.rot90(blockShape, -3)
+        blockInfluences = np.rot90(blockInfluences, -3)
+        if settableCheck(blockShape, boardMine, x, y):
+            changeBoardImage(blockShape, colorImage, colorRect, x, y, surface, tileLength)
+            changeBoardStatus(blockShape, blockInfluences, boardMine, boardOpponent, x, y)
             return True
 
 if __name__ == '__main__':
