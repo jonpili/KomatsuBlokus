f894e58c634c1d474a7238116fd9820b941c2c1a
JoeTakasuna <jonpili99@gmail.com>
2019-06-01 08:29:57 +0900

    pブロック実装&選択可能に

--- blokus.py
+++ blokus.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from pieces import a_block
 from pieces import b_block
+from pieces import p_block
 
 tileLength = 50
 tileNumber = 8
@@ -120,6 +121,10 @@ def main():
                             if b_block.main(greenImage, greenRect, boardGreen, boardYellow, selectedDirection, xpos, ypos, surface, tileLength):
                                 whoTurn = checkBoard(GREEN)
                                 selectedBlock, selectedDirection = selectBlock()
+                        elif selectedBlock == 'p':
+                            if p_block.main(greenImage, greenRect, boardGreen, boardYellow, selectedDirection, xpos, ypos, surface, tileLength):
+                                whoTurn = checkBoard(GREEN)
+                                selectedBlock, selectedDirection = selectBlock()
 
                 elif whoTurn == YELLOW:
                     if boardYellow[ypos][xpos] != CANTSET:
@@ -129,6 +134,10 @@ def main():
                                 selectedBlock, selectedDirection = selectBlock()
                         elif selectedBlock == 'b':
                             if b_block.main(yellowImage, yellowRect, boardYellow, boardGreen, selectedDirection, xpos, ypos, surface, tileLength):
+                                whoTurn = checkBoard(YELLOW)
+                                selectedBlock, selectedDirection = selectBlock()
+                        elif selectedBlock == 'p':
+                            if p_block.main(yellowImage, yellowRect, boardYellow, boardGreen, selectedDirection, xpos, ypos, surface, tileLength):
                                 whoTurn = checkBoard(YELLOW)
                                 selectedBlock, selectedDirection = selectBlock()
 
