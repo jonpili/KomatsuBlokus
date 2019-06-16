99632e2309e68e32f61ac5ffd214f5033dc49954
JoeTakasuna <jonpili99@gmail.com>
2019-06-16 13:24:14 +0900

    testを少し使った

--- test.py
+++ test.py
@@ -1,16 +1,34 @@
-import numpy as np
+# import numpy as np
+#
+# array = [
+#         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#         [1, 0, 2, 1, 2, 0, 0, 0, 0, 1],
+#         [1, 0, 1, 1, 1, 0, 0, 0, 0, 1],
+#         [1, 0, 1, 1, 1, 0, 0, 0, 0, 1],
+#         [1, 2, 1, 1, 1, 1, 0, 0, 0, 1],
+#         [1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
+#         [1, 2, 1, 1, 2, 1, 2, 0, 0, 1],
+#         [1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
+#         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+#         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+#         ]
+#
+# array = np.asarray(array).T
+# print(array[2][1])
 
-array = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 2, 1, 2, 0, 0, 0, 0, 1],
-        [1, 0, 1, 1, 1, 0, 0, 0, 0, 1],
-        [1, 0, 1, 1, 1, 0, 0, 0, 0, 1],
-        [1, 2, 1, 1, 1, 1, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
-        [1, 2, 1, 1, 2, 1, 2, 0, 0, 1],
-        [1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+import re
 
-array = np.asarray(array).T
+while True:
+    selectedBlock = input('ブロックを選択してください：')
+    while re.search('[^a-u]', selectedBlock) or re.search('[a-u]{2,}', selectedBlock) or selectedBlock == '':
+        print('入力が間違っています')
+        selectedBlock = input('ブロックを選択してください：')
 
-print(array[2][1])
+    selectedDirection = input('向きを選択してください：')
+    while re.search('[^0-7]', selectedDirection) or re.search('[0-7]{2,}', selectedDirection) or selectedDirection == '':
+        print('入力が間違っています')
+        selectedDirection = input('向きを選択してください：')
+    selectedDirection = int(selectedDirection)
+
+    print(selectedBlock)
+    print(selectedDirection)
