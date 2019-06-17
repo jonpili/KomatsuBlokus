7661ec04e03863cf2f159c8e34dc3f59dfacbfed
JoeTakasuna <jonpili99@gmail.com>
2019-06-17 14:27:32 +0900

    testでコンソール上のブロック表示テスト

--- test.py
+++ test.py
@@ -1,17 +1,36 @@
-# import numpy as np
-#
-# array = [
-#         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-#         [1, 0, 2, 1, 2, 0, 0, 0, 0, 1],
-#         [1, 0, 1, 1, 1, 0, 0, 0, 0, 1],
-#         [1, 0, 1, 1, 1, 0, 0, 0, 0, 1],
-#         [1, 2, 1, 1, 1, 1, 0, 0, 0, 1],
-#         [1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
-#         [1, 2, 1, 1, 2, 1, 2, 0, 0, 1],
-#         [1, 0, 0, 0, 1, 1, 0, 0, 0, 1],
-#         [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-#         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-#         ]
-#
-# array = np.asarray(array).T
-# print(array[2][1])
+import numpy as np
+
+blockShape = [
+             [0,0,1,0,0],
+             [0,0,1,0,0],
+             [0,0,1,0,0],
+             [0,1,1,0,0],
+             [0,0,0,0,0]
+             ]
+
+blockShape = np.asarray(blockShape)
+print(blockShape)
+
+blockCenter = round(len(blockShape)/2)
+
+# 上の枠
+print('　', end='')
+for i in range(len(blockShape)):
+    print('＿', end='')
+print('　')
+
+# 1を黒四角に、0を空白に置換
+for lineIndex, line in enumerate(np.where(blockShape > 0, '䨻', '　')):
+    print('｜', end='')
+    for colIndex, col in enumerate(line):
+        if lineIndex == blockCenter and colIndex == blockCenter:
+            print('口', end='')
+        else:
+            print(col, end='')
+    print('｜')
+print('　', end='')
+
+# 下の枠
+for i in range(len(blockShape)):
+    print('￣', end='')
+print('　')
