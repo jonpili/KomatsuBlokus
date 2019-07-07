83558a02637f5f133f43a26c83e26d259eaa5e90
JoeTakasuna <jonpili99@gmail.com>
2019-07-07 13:15:35 +0900

    pointBlockをclass内に格納

--- simpleBlokus/main.py
+++ simpleBlokus/main.py
@@ -16,25 +16,7 @@ screenWidth  = tileLength * (tileNumber + 2)
 screenHeight = tileLength * (tileNumber + 2)
 tileLimit    = tileLength * tileNumber
 
-def makeBoard():
-    board  = [[BLANK for width in range(tileNumber + 2)] for height in range(tileNumber + 2)]
-    # 枠を作成
-    for i in range(tileNumber + 2):
-        board[0][i]              = CANTSET
-        board[tileNumber + 1][i] = CANTSET
-    for i in range(tileNumber):
-        board[i + 1][0]              = CANTSET
-        board[i + 1][tileNumber + 1] = CANTSET
-    return board
 
-# 初期位置を設定
-boardGreen = makeBoard()
-boardGreen[2][2] = ABLESET
-
-boardYellow = makeBoard()
-boardYellow[4][4] = ABLESET
-
-surface = pygame.display.set_mode((screenWidth, screenHeight))
 
 def changeTileStatus(x, y, board1, board2):
     # ブロック自体を左上から時計回りに
@@ -62,23 +44,14 @@ def changeTileStatus(x, y, board1, board2):
     # ブロック自体を左上から時計回りに
     board2[y][x] = CANTSET
 
-def pointBlock(x, y, color, colorImage, colorRect):
-    if color == GREEN:
-        if boardGreen[y][x] == ABLESET:
-            surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
-            changeTileStatus(x, y, boardGreen, boardYellow)
-            return True
 
-    elif color == YELLOW:
-        if boardYellow[y][x] == ABLESET:
-            surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
-            changeTileStatus(x, y, boardYellow, boardGreen)
-            return True
 
 class Game():
+    pygame.init()
+    surface = pygame.display.set_mode((screenWidth, screenHeight))
+
     def __init__(self):
-        pygame.init()
-        surface.fill((0,0,0)) # 黒で塗りつぶし
+        self.surface.fill((0,0,0)) # 黒で塗りつぶし
         pygame.display.set_caption('Mini Blokus')
 
         self.tileImage   = pygame.image.load('image/tile.bmp').convert()
@@ -95,7 +68,25 @@ class Game():
         for i in range(0, tileLimit, tileLength):
             for j in range(0, tileLimit, tileLength):
                 # 枠の分はスキップ
-                surface.blit(self.tileImage, self.tileRect.move((i + tileLength), (j + tileLength)))
+                self.surface.blit(self.tileImage, self.tileRect.move((i + tileLength), (j + tileLength)))
+
+        # 初期位置を設定
+        self.boardGreen = self.makeBoard()
+        self.boardGreen[2][2] = ABLESET
+
+        self.boardYellow = self.makeBoard()
+        self.boardYellow[4][4] = ABLESET
+
+    def makeBoard(self):
+        board  = [[BLANK for width in range(tileNumber + 2)] for height in range(tileNumber + 2)]
+        # 枠を作成
+        for i in range(tileNumber + 2):
+            board[0][i]              = CANTSET
+            board[tileNumber + 1][i] = CANTSET
+        for i in range(tileNumber):
+            board[i + 1][0]              = CANTSET
+            board[i + 1][tileNumber + 1] = CANTSET
+        return board
 
     def start(self):
         whoTurn = self.checkBoard(GREEN)
@@ -110,22 +101,22 @@ class Game():
                     xpos = int(pygame.mouse.get_pos()[0]/tileLength) # 右方向に正
                     ypos = int(pygame.mouse.get_pos()[1]/tileLength) # 下方向に正
                     if whoTurn == GREEN:
-                        if boardGreen[ypos][xpos] != CANTSET:
-                            if pointBlock(xpos, ypos, GREEN, self.greenImage, self.greenRect):
+                        if self.boardGreen[ypos][xpos] != CANTSET:
+                            if self.pointBlock(xpos, ypos, GREEN, self.greenImage, self.greenRect):
                                 whoTurn = self.checkBoard(YELLOW)
 
                     elif whoTurn == YELLOW:
-                        if boardYellow[ypos][xpos] != CANTSET:
-                            if pointBlock(xpos, ypos, YELLOW, self.yellowImage, self.yellowRect):
+                        if self.boardYellow[ypos][xpos] != CANTSET:
+                            if self.pointBlock(xpos, ypos, YELLOW, self.yellowImage, self.yellowRect):
                                 whoTurn = self.checkBoard(GREEN)
 
     def checkBoard(self, color):
         print('')
         print('ーーーー緑の盤面ーーーー')
-        for width in boardGreen:
+        for width in self.boardGreen:
             print(width)
         print('ーーーー黄の盤面ーーーー')
-        for width in boardYellow:
+        for width in self.boardYellow:
             print(width)
 
         if color == GREEN:
@@ -135,6 +126,19 @@ class Game():
 
         pygame.display.flip()
         return color
+
+    def pointBlock(self, x, y, color, colorImage, colorRect):
+        if color == GREEN:
+            if self.boardGreen[y][x] == ABLESET:
+                self.surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
+                changeTileStatus(x, y, self.boardGreen, self.boardYellow)
+                return True
+
+        elif color == YELLOW:
+            if self.boardYellow[y][x] == ABLESET:
+                self.surface.blit(colorImage, colorRect.move(tileLength * x, tileLength * y))
+                changeTileStatus(x, y, self.boardYellow, self.boardGreen)
+                return True
 
 def main():
     game = Game()
