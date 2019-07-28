75161b0c8bda9a4c0a602e6ad68c300f65eec6e5
JoeTakasuna <jonpili99@gmail.com>
2019-07-28 20:04:53 +0900

    select_block_by_CPの実装

--- Player.py
+++ Player.py
@@ -1,4 +1,5 @@
 import Block
+import random
 
 class Player():
     block_shape_index_list     = [chr(ord('a') + i) for i in range(21)] # aからuの配列
@@ -19,10 +20,17 @@ class Player():
 
     def start_my_turn(self, game, board):
         board.check_status(game)
-        block = self.select_block(board)
+        if self.color == game.COLOR_LIST[0]:
+            block = self.select_block(board)
+        elif self.color == game.COLOR_LIST[1]:
+            block = self.select_block_by_CP(board)
+
         while not board.settable_area_exist_check(self.color, block.selected['shape']):
             print('そのブロックを置く場所がありません\n')
-            block = self.select_block(board)
+            if self.color == game.COLOR_LIST[0]:
+                block = self.select_block(board)
+            elif self.color == game.COLOR_LIST[1]:
+                block = self.select_block_by_CP(board)
         return block
 
     def select_block(self, board):
@@ -38,6 +46,14 @@ class Player():
             print('入力が間違っています')
             self.selected_direction_index = input('向きを選択してください：')
         self.selected_direction_index = int(self.selected_direction_index)
+
+        block = Block.Block(self.selected_shape_index, self.selected_direction_index, True)
+        return block
+
+    def select_block_by_CP(self, board):
+        usable_block_list = [i for i in self.block_shape_index_list if i not in self.used_blocks]
+        self.selected_shape_index = random.choice(usable_block_list)
+        self.selected_direction_index = random.choice(range(8))
 
         block = Block.Block(self.selected_shape_index, self.selected_direction_index, True)
         return block
