14de7cb304c25fb8334dacaf39fd87f6d7275cf7
JoeTakasuna <jonpili99@gmail.com>
2019-07-28 15:24:57 +0900

    テスト

--- test.py
+++ test.py
@@ -1,28 +1,34 @@
-# import numpy as np
-# import test1
-#
+import numpy as np
+
 # array = [
-#         [0,0,1,0,0],
-#         [0,0,1,0,0],
-#         [0,0,1,0,0],
-#         [0,1,1,0,0],
-#         [0,0,0,0,0]
+#         [1,2,3,4,5],
+#         [1,2,3,4,5],
+#         [1,2,3,4,5],
+#         [1,2,3,4,5],
+#         [1,2,3,4,5]
 #         ]
 #
 # array = np.asarray(array)
-#
-#我こそは村松
 
-class Oya():
-    word = 'こんにちは'
-    def talk(self):
-        b.talk()
+BLANK   = 0 # ブロックは置かれていない
+CANTSET = 1 # ブロックが置かれている or 自分のブロックが隣接している
+ABLESET = 2 # 自分のブロックが角で接している
 
-class Kodomo():
-    def talk(self):
-        print(a.word)
+TILE_NUMBER = 8
 
-a = Oya()
-b = Kodomo()
+def make_board():
+    board  = [[[BLANK, BLANK] for width in range(TILE_NUMBER + 2)] for height in range(TILE_NUMBER + 2)]
+    # 枠を作成
+    for i in range(TILE_NUMBER + 2):
+        board[0][i]              = [CANTSET, CANTSET]
+        board[TILE_NUMBER + 1][i] = [CANTSET, CANTSET]
+    for i in range(TILE_NUMBER):
+        board[i + 1][0]              = [CANTSET, CANTSET]
+        board[i + 1][TILE_NUMBER + 1] = [CANTSET, CANTSET]
+    board = np.asarray(board)
+    return board
 
-a.talk()
+board = make_board()
+
+green_board = list(map(lambda x: list(map(lambda y: y[1], x)), board))
+print(green_board)
