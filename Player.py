af3c3fbbc5ff14217c7f8d737b5837d538f17318
JoeTakasuna <jonpili99@gmail.com>
2019-07-31 13:25:46 +0900

    select_blockとselect_block_by_CPの統合

--- Player.py
+++ Player.py
@@ -22,36 +22,32 @@ class Player():
 
     def start_my_turn(self, game, board):
         board.check_status(game)
-        if self.computer: block = self.select_block_by_CP(board)
-        else:             block = self.select_block(board)
+        block = self.select_block(board)
 
-        while not board.settable_area_exist_check(self.color, block.selected['shape']):
-            print('そのブロックを置く場所がありません\n')
-            if self.computer: block = self.select_block_by_CP(board)
-            else:             block = self.select_block(board)
+        if not self.computer:
+            while not board.settable_area_exist_check(self.color, block.selected['shape']):
+                print('そのブロックを置く場所がありません\n')
+                block = self.select_block(board)
         return block
 
     def select_block(self, board):
-        print('手持ちのブロックリスト')
-        print([i for i in self.block_shape_index_list if i not in self.used_blocks])
-        self.selected_shape_index = input('\nブロックを選択してください：')
-        while True:
-            if self.check_input(board):
-                break
+        if self.computer:
+            use_block = random.choice(self.usable_blocks)
+            self.selected_shape_index = use_block[0]
+            self.selected_direction_index = use_block[1]
+        else:
+            print('手持ちのブロックリスト')
+            print([i for i in self.block_shape_index_list if i not in self.used_blocks])
+            self.selected_shape_index = input('\nブロックを選択してください：')
+            while True:
+                if self.check_input(board):
+                    break
 
-        self.selected_direction_index = input('向きを選択してください：')
-        while not self.selected_direction_index in self.block_direction_index_list:
-            print('入力が間違っています')
             self.selected_direction_index = input('向きを選択してください：')
-        self.selected_direction_index = int(self.selected_direction_index)
-
-        block = Block.Block(self.selected_shape_index, self.selected_direction_index, True)
-        return block
-
-    def select_block_by_CP(self, board):
-        use_block = random.choice(self.usable_blocks)
-        self.selected_shape_index = use_block[0]
-        self.selected_direction_index = use_block[1]
+            while not self.selected_direction_index in self.block_direction_index_list:
+                print('入力が間違っています')
+                self.selected_direction_index = input('向きを選択してください：')
+            self.selected_direction_index = int(self.selected_direction_index)
 
         block = Block.Block(self.selected_shape_index, self.selected_direction_index, True)
         return block
