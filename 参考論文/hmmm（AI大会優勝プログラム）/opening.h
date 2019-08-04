b95e8f5ddbbf436c03cfa1e92bc0f2c85f09af7a
JoeTakasuna <jonpili99@gmail.com>
2019-08-04 13:21:34 +0900

    参考コードを圧縮

--- 参考論文/hmmm（AI大会優勝プログラム）/opening.h
+++
@@ -1,9 +0,0 @@
-#ifndef OPENING_H_
-#define OPENING_H_
-
-extern const unsigned short violet_first_moves[];
-extern const unsigned short orange_first_moves[];
-
-Move opening_move(Board* b, const Move* history);
-
-#endif // OPENING_H_
